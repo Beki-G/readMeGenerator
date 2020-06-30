4e711f448cb49ed1d671301108e0129b6ea28421
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-06-30 15:02:37 -0700

    add initial questions for user input and test with inquirer prompts

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
-const questions = [
+const inquirer = require('inquirer');
 
+const questions = [
+    {
+        name: "title",
+        message: "What is the name of the project?",
+        type:"input"   
+    },
+    {
+        name:"description",
+        message: "What is the description of the project?",
+        type:"input"
+    },
+    {
+        name:"installation",
+        message: "What are the installation instructions?",
+        type:"input"
+    },
+    {
+        name: "usageInfo",
+        message: "Why would you use this?",
+        type:"input"
+    },
+    {
+        name: "contribution",
+        message: "What are teh contribution guidelines",
+        type: "input"
+    },
+    {
+        name: "test",
+        message: "What are thes test instructions?",
+        type:"input"
+    },
+    {
+        name:"license",
+        message: "What type of license do you want to add?",
+        type: "list",
+        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause \"Simplified\" license", "BSD 3-Clause \"New\" or \"Revised\" License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 1.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
+    },
+    {
+        name:"githubUsername",
+        message: "What is your Github username?",
+        type: "input"
+    },
+    {
+        name:"email",
+        message: "What email can people reach out to you for addtional questions?",
+        type:"input"
+    }
 ];
 
 function writeToFile(fileName, data) {
 }
 
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then(response=>{
+            console.log(response)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
 }
 
 init();
